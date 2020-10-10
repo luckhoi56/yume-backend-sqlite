ecf2566796d8b08c51abc7ef935402bf530a28eb
Eric Luc <luckhoi89@gmail.com>
2020-10-10 00:43:54 -0700

    change to backend

--- backend.js
+++ backend.js
@@ -12,7 +12,7 @@ app.use(cors())
 
   
 
-  app.get("/menu", (req, res, next) => {
+  app.get("/menuItem", (req, res, next) => {
     var sql = 'SELECT * from menu';
     var params=[];
     db.all(sql,params,(err,rows) =>{
@@ -28,7 +28,21 @@ app.use(cors())
     
    });
   
-  
+   app.get("/menuCategory", (req, res, next) => {
+    var sql = 'SELECT distinct category from menu';
+    var params=[];
+    db.all(sql,params,(err,rows) =>{
+      if(err){
+        res.status(400).json({"error": err.message});
+        return;
+      }
+      res.json({
+        "message":"success",
+        "data":rows
+      })
+    })
+    
+   });
    
   //app.listen(4242, () => console.log('Node server listening on port 4242!'));
   const PORT = process.env.PORT || 4242;
